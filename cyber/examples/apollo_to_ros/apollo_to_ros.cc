285cd65485fd5910dfeb2b1ed28a5676c4c216a5
luy <luyi1990@126.com>
2021-03-06 09:51:23 +0800

    update apollo to ros demo

--- cyber/examples/apollo_to_ros/apollo_to_ros.cc
+++ cyber/examples/apollo_to_ros/apollo_to_ros.cc
@@ -14,20 +14,75 @@
  * limitations under the License.
  *****************************************************************************/
 #include "cyber/examples/apollo_to_ros/apollo_to_ros.h"
-#include <thread>
 #include <sched.h>
 #include <sys/resource.h>
 #include <sys/syscall.h>
+#include <thread>
 
 using apollo::cyber::Time;
 
+ApolloToRos::~ApolloToRos() {}
+
 bool ApolloToRos::Init() {
   AINFO << "ApolloToRos init";
+
+  uint16_t server_port = 11435;
+  apollo::cyber::scheduler::Instance()->CreateTask(
+      [&server_port]() {
+        struct sockaddr_in server_addr;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+        server_addr.sin_port = htons(server_port);
+
+        Session session;
+        session.Socket(AF_INET, SOCK_STREAM, 0);
+        if (session.Bind((struct sockaddr*)&server_addr, sizeof(server_addr)) <
+            0) {
+          std::cout << "bind to port[" << server_port << "] failed."
+                    << std::endl;
+          return;
+        }
+        session.Listen(10);
+        auto conn_session = session.Accept((struct sockaddr*)nullptr, nullptr);
+        AINFO << "accepted";
+        auto routine_name =
+            "connected session" + std::to_string(Time::Now().ToNanosecond());
+        apollo::cyber::scheduler::Instance()->CreateTask(
+            std::bind(&ApolloToRos::SendData, conn_session), routine_name);
+      },
+      "to_ros_server");
+
   return true;
 }
 
 bool ApolloToRos::Proc(const std::shared_ptr<PointCloud>& point_cloud) {
-  AINFO << "frame id:" << point_cloud->frame_id() << ", width:" << point_cloud->width()
+  AINFO << "frame id:" << point_cloud->frame_id()
+        << ", width:" << point_cloud->width()
         << ", height:" << point_cloud->height();
+  queue_.Put(point_cloud);
   return true;
+}
+
+void ApolloToRos::SendData(const std::shared_ptr<Session>& session) {
+  std::list<std::shared_ptr<PointCloud> > list;
+  queue_.Take(list);
+
+  for (auto point_cloud : list) {
+    std::string point_cloud_str;
+    if (!point_cloud->SerializeToString(&point_cloud_str)) {
+      AINFO << "SerializeToString failed!";
+      return;
+    }
+    int nbytes = static_cast<int>(
+        session->Write(point_cloud_str.c_str(), point_cloud_str.size()));
+    if (nbytes == 0) {
+      AINFO << "client has been closed.";
+      session->Close();
+    }
+
+    if (nbytes < 0) {
+      AINFO << "send to client failed.";
+      session->Close();
+    }
+  }
 }
--- cyber/examples/apollo_to_ros/apollo_to_ros.h
+++ cyber/examples/apollo_to_ros/apollo_to_ros.h
@@ -15,17 +15,112 @@
  *****************************************************************************/
 #include <memory>
 
-#include "cyber/cyber.h"
 #include "cyber/component/component.h"
+#include "cyber/cyber.h"
+#include "cyber/io/session.h"
+#include "cyber/scheduler/scheduler_factory.h"
+#include "cyber/task/task.h"
+
 #include "modules/drivers/proto/pointcloud.pb.h"
 
 using apollo::cyber::Component;
 using apollo::cyber::ComponentBase;
 using apollo::drivers::PointCloud;
+using apollo::cyber::io::Session;
+
+template <typename T>
+class SyncQueue {
+ public:
+  SyncQueue() : m_maxSize(32), m_needStop(false) {}
+  SyncQueue(int maxSize) : m_maxSize(maxSize), m_needStop(false) {}
+
+  void Put(const T& x) { Add(x); }
+  void Put(const T&& x) { Add(std::forward<T>(x)); }
+
+  void Take(std::list<T>& list) {
+    std::unique_lock<std::mutex> locker(m_mutex);
+    m_notEmpty.wait(locker, [this] { return (m_needStop || NotEmpty()); });
+    if (m_needStop) return;
+    list = std::move(m_queue);
+    m_notFull.notify_one();
+  }
+
+  void Take(T& t) {
+    std::unique_lock<std::mutex> locker(m_mutex);
+    m_notEmpty.wait(locker, [this] { return (m_needStop || NotEmpty()); });
+    if (m_needStop) return;
+    t = m_queue.front();
+    m_queue.pop_front();
+    m_notFull.notify_one();
+  }
+
+  void Stop() {
+    {
+      std::lock_guard<std::mutex> locker(m_mutex);
+      m_needStop = true;
+    }
+    m_notFull.notify_all();
+    m_notEmpty.notify_all();
+  }
+
+  bool Empty() {
+    std::lock_guard<std::mutex> locker(m_mutex);
+    return m_queue.empty();
+  }
+
+  bool Full() {
+    std::lock_guard<std::mutex> locker(m_mutex);
+    return m_queue.size() == m_maxSize;
+  }
+
+  size_t Size() {
+    std::lock_guard<std::mutex> locker(m_mutex);
+    return m_queue.size();
+  }
+
+  int Count() { return m_queue.size(); }
+
+ private:
+  bool NotFull() const { return (static_cast<int>(m_queue.size()) < m_maxSize); }
+  bool NotEmpty() const { return !m_queue.empty(); }
+
+  void Add(const T& x) {
+    std::unique_lock<std::mutex> locker(m_mutex);
+    m_notFull.wait(locker, [this] { return (m_needStop || NotFull()); });
+    if (m_needStop) return;
+    m_queue.push_back(x);
+    m_notEmpty.notify_one();
+  }
+
+  template <typename F>
+  void Add(F&& x) {
+    std::unique_lock<std::mutex> locker(m_mutex);
+    m_notFull.wait(locker, [this] { return (m_needStop || NotFull()); });
+    if (m_needStop) return;
+    m_queue.push_back(std::forward<F>(x));
+    m_notEmpty.notify_one();
+  }
+
+ private:
+  std::list<T> m_queue;
+  std::mutex m_mutex;
+  std::condition_variable m_notEmpty;
+  std::condition_variable m_notFull;
+  int m_maxSize;
+  bool m_needStop;
+};
 
 class ApolloToRos : public Component<PointCloud> {
  public:
+  ~ApolloToRos();
+
   bool Init() override;
   bool Proc(const std::shared_ptr<PointCloud>& point_cloud) override;
+
+ private:
+  void SendData(const std::shared_ptr<Session>& session);
+
+ private:
+  SyncQueue<std::shared_ptr<PointCloud> > queue_;
 };
 CYBER_REGISTER_COMPONENT(ApolloToRos)
